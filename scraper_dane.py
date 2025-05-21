da11091f2b5da4b545434a0b0bbc8170b83454a6
Ronald Guerra <ronaldg0304@gmail.com>
2025-05-20 20:27:28 -0500

    Primer commit

---
+++ scraper_dane.py
@@ -0,0 +1,65 @@
+import requests
+from bs4 import BeautifulSoup
+import time
+from datetime import datetime
+
+
+
+# Telegram config
+TOKEN = '7659076432:AAGX_1_GB6tO-fIG0TcgZTUO0KakVhoMNCw'
+CHAT_ID = '866289125'
+
+# Función para enviar mensaje a Telegram
+def enviar_telegram(mensaje):
+
+    print("¡Mensaje enviado correctamente!")
+    url = f'https://api.telegram.org/bot{TOKEN}/sendMessage'
+    payload = {
+        'chat_id': CHAT_ID,
+        'text': mensaje
+    }
+    requests.post(url, data=payload)
+
+# URL de convocatorias del DANE
+url = "https://bpso.dane.gov.co/invitations/detailed"
+
+# Lista para almacenar invitaciones conocidas
+lista_invitaciones = []
+
+def revisar_convocatorias():
+    response = requests.get(url)
+    count = 0
+
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text, 'html.parser')
+        invitaciones = soup.find_all('h4')
+        departamentos = soup.find_all('h5')
+
+        nuevas = []
+
+        for i in invitaciones:
+            if 'Invitación' in i.text and i.text not in lista_invitaciones:
+                lista_invitaciones.append(i.text)
+                nuevas.append(i.text)
+
+        for invitacion in nuevas:
+            mensaje = f"📢 Nueva invitación publicada:\n\n{invitacion}"
+            enviar_telegram(mensaje)
+
+        for d in departamentos:
+            if d.text.lower() == 'sincelejo':
+                count += 1
+
+
+        enviar_telegram(f"📍 Convocatorias en Sincelejo: {count} ")
+        count = 0
+    else:
+        print("Error al acceder a la página:", response.status_code)
+
+# Ejecutar la revisión cada hora
+while True:
+    hora_actual = datetime.now().strftime("%H:%M:%S")
+    fecha_actual = datetime.now().strftime("%d/%m/%Y")
+    revisar_convocatorias()
+    enviar_telegram(f"🔁 {fecha_actual}- Ultima revisión a las {hora_actual}")
+    time.sleep(60)  # 3600 segundos = 1 hora
